ce6e570450e36547c8f3bd69567153a66ba4763f
YongPilMoon <m71661@gmail.com>
2017-10-04 13:08:25 +0900

    Fix users controller to fetch one week data

--- dashboardPrj-backend/src/api/controller.js
+++ dashboardPrj-backend/src/api/controller.js
@@ -3,12 +3,44 @@ const _ = require('lodash');
 
 module.exports = {
   users: (req, res) => {
-    const startDay = req.params.startDay;
-    const finishDay = req.params.finishDay;
-    const startDayTS = new Date(startDay).getTime();
-    const finishDayTS = new Date(finishDay).getTime();
-    const period = user => user.date >= startDayTS && user.date <= finishDayTS;
-    const filteredUsers = _(users).filter(period);
+    const untilDate = req.params.untilDate;
+    /**
+     * 한국 시간으로 변환 했을 timezone에 맞게 변형해 준다.
+     * ex) 2017-9-27을 getTime을 통해 unixtime을 얻으면 UTC의 2017-9-27일이고
+     * 한국 timezone으로 변환하면 2017-09-27 +9시가 돤다.
+     * 때문에 실제 한국 timezone으로 변환 했을 때 2017-9-21이 되는 시간을 얻으려면 9시간을 빼주어야 한다.
+     */
+    const finishDateTS = new Date(untilDate).getTime() - 9 * 3600000 + 12 * 3600000;
+    const startDateTS = finishDateTS - 3600000 * 24 * 7;
+
+    const filterPeriod = user => user.date >= startDateTS && user.date <= finishDateTS;
+    /* 가입시간을 시간단위로 자르는 함수 */
+    const roundDownToHour = (user) => {
+      const dateObj = new Date(user.date);
+      const year = dateObj.getFullYear();
+      const month = dateObj.getMonth() + 1;
+      const date = dateObj.getDate();
+      const hours = dateObj.getHours();
+      return `${year}-${month}-${date}-${hours}`;
+    };
+
+    const makeUserPerHourObj = (users, key) => {
+      const [androidUsers, iosUsers] = _.partition(users, user => _.includes(user.device, "Android"));
+      const [year, month, date, hours] = key.split("-");
+      return {
+        "date": `${year}-${month}-${date}`,
+        "time": `${hours}`,
+        "totalUserCnt": users.length,
+        "androidUserCnt": androidUsers.length,
+        "iosUserCnt": iosUsers.length
+      }
+    };
+
+    const filteredUsers = _(users)
+    .filter(filterPeriod)
+    .groupBy(roundDownToHour)
+    .map(makeUserPerHourObj);
     res.json(filteredUsers);
   }
 };
+
--- dashboardPrj-backend/src/api/index.js
+++ dashboardPrj-backend/src/api/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-router.get('/users/from/:startDay/to/:finishDay', controller.users);
+router.get('/users/sevenData/:untilDate', controller.users);
 
 module.exports = router;
